4ed9502fa78ff5290fe5f34ebee81833ab3f3abe
L9029 <angeloca03@gmail.com>
2023-07-22 14:46:34 -0400

    Building the 'moving-right' function for the game.

---
+++ func/moves.py
@@ -0,0 +1,8 @@
+#Moving Right
+def moveR(board, blank_cell_index, num_cols):
+    if blank_cell_index % num_cols == 0:
+        return blank_cell_index
+    
+    board[blank_cell_index - 1], board[blank_cell_index] = board[blank_cell_index], board[blank_cell_index - 1]
+    
+    return blank_cell_index - 1
