e7329f4ca29bac0b86d74f5387564d12bbb3147d
L9029 <angeloca03@gmail.com>
2023-07-22 15:44:45 -0400

    Building the 'moving-left' function for the game.

--- func/moves.py
+++ func/moves.py
@@ -6,3 +6,12 @@ def moveR(board, blank_cell_index, num_cols):
     board[blank_cell_index - 1], board[blank_cell_index] = board[blank_cell_index], board[blank_cell_index - 1]
     
     return blank_cell_index - 1
+
+#Moving Left
+def moveR(board, blank_cell_index, num_cols):
+    if (blank_cell_index + 1) % num_cols == 0:
+        return blank_cell_index
+    
+    board[blank_cell_index + 1], board[blank_cell_index] = board[blank_cell_index], board[blank_cell_index + 1]
+    
+    return blank_cell_index + 1
