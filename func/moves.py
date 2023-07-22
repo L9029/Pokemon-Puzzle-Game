3cf9cd62567e935eae05362837a3ca121d9dde04
L9029 <angeloca03@gmail.com>
2023-07-22 16:30:28 -0400

    Building the 'moving-up' function for the game.

--- func/moves.py
+++ func/moves.py
@@ -8,7 +8,7 @@ def moveR(board, blank_cell_index, num_cols):
     return blank_cell_index - 1
 
 #Moving Left
-def moveR(board, blank_cell_index, num_cols):
+def moveL(board, blank_cell_index, num_cols):
     if (blank_cell_index + 1) % num_cols == 0:
         return blank_cell_index
     
@@ -17,10 +17,20 @@ def moveR(board, blank_cell_index, num_cols):
     return blank_cell_index + 1
 
 #Moving Down
-def moveR(board, blank_cell_index, num_cols):
+def moveD(board, blank_cell_index, num_cols):
     if blank_cell_index < num_cols:
         return blank_cell_index
     
     board[blank_cell_index - num_cols], board[blank_cell_index] = board[blank_cell_index], board[blank_cell_index - num_cols]
     
     return blank_cell_index - num_cols
+
+
+#Moving Up
+def moveU(board, blank_cell_index, num_cols, num_rows):
+    if blank_cell_index >= (num_rows - 1) * num_cols:
+        return blank_cell_index
+    
+    board[blank_cell_index + num_cols], board[blank_cell_index] = board[blank_cell_index], board[blank_cell_index + num_cols]
+    
+    return blank_cell_index + num_cols
